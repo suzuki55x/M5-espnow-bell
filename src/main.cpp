0c04efb97358e408821741bea6bd6a86e3b6b35e
T. Suzuki <st09d18@icloud.com>
2022-07-23 01:53:35 +0900

    Init chime sample

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <M5StickCPlus.h>
+
+// ピンポン(ボタン押した時)
+void chime() {
+  M5.Beep.tone(659, 500);
+  M5.Beep.tone(523, 1000);
+}
+
+void setup() {
+  M5.begin();
+  M5.Beep.setBeep(4000, 100);// init
+}
+
+void loop() {
+  M5.update();// ボタンの状態更新
+  M5.Beep.update();// tone関数の経過秒数更新
+
+  if (M5.BtnA.wasPressed()) {
+    chime();
+  }
+}
